1be65b57e70c2d00204ea586be2ac204e64e792a
AkritiSubedi <aakusubedi075@gmail.com>
2023-11-02 20:46:52 +0545

    final

--- lib/page/cart_page.dart
+++ lib/page/cart_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../components/cart_item.dart';
+import '../models/shoe.dart';
+
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('cart '),);
+    return Consumer(
+        builder: (context, value, child) =>const  Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                children: [
+                  // heading
+                  const Text(
+                    'My Cart',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                 
+                ],
+              ),
+            ));
   }
 }
